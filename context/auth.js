2a39b61e3022423174f5263947333ca95000d01c
klavenjones <klavensjones@gmail.com>
2021-01-10 21:00:14 -0500

    Added Firebase

---
+++ context/auth.js
@@ -0,0 +1,32 @@
+import React, { useEffect, useContext, createContext, useState } from 'react'
+import nookies from 'nookies'
+
+import firebaseClient from '../util/firebase/firebaseClient'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+
+const AuthContext = createContext({})
+
+export const AuthProvider = ({ children }) => {
+  firebaseClient()
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    return firebase.auth().onIdTokenChanged(async (user) => {
+      if (!user) {
+        setUser(null)
+        nookies.set(undefined, 'token', '', {})
+        return
+      }
+      const token = await user.getIdToken()
+      setUser(user)
+      nookies.set(undefined, 'token', token, {})
+    })
+  }, [])
+
+  return (
+    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+  )
+}
+
+export const useAuth = () => useContext(AuthContext)
---
+++ pages/register.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Navigation, Register } from '../components'
+
+export default function RegisterPage() {
+  return (
+    <>
+      <Navigation register/>
+      <div className='min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-md'>
+          <img
+            className='mx-auto h-12 w-auto'
+            src='https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg'
+            alt='Workflow'
+          />
+          <h2 className='mt-6 text-center text-3xl font-extrabold text-gray-900'>
+            Sign up for free
+          </h2>
+          <p className='mt-2 text-center text-sm text-gray-600 max-w'>
+           Already have an account?{'  '}
+            <a
+              href='/login'
+              className='font-medium text-indigo-600 hover:text-indigo-500'
+            >
+              Login
+            </a>
+          </p>
+        </div>
+        <Register />
+      </div>
+    </>
+  )
+}
