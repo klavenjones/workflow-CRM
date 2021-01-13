2d22e4d48b6add20a5a2855bde64e76383edae4a
Klaven Jones <klavensjones@gmail.com>
2021-01-13 13:10:21 -0500

    Did work on Dashboard

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import nookies from 'nookies'
 
 import { SideNav } from '../../components'
+import { FaPlus } from 'react-icons/fa'
 
 import { verifyIdToken } from '../../util/firebase/firebaseAdmin'
 import firebaseClient from '../../util/firebase/firebaseClient'
@@ -15,7 +16,6 @@ function Dashboard({ session }) {
       <SideNav page='dashboard'>
         {/* PAGE HEADER */}
         <div className='flex flex-col overflow-hidden'>
-          {/* This example requires Tailwind CSS v2.0+ */}
           <div className='pb-5 border-b border-gray-200 sm:flex sm:items-center sm:justify-between'>
             <h3 className='text-2xl leading-6 font-xl text-gray-900'>
               Dashboard
@@ -35,63 +35,49 @@ function Dashboard({ session }) {
               </button>
             </div>
           </div>
-          {/* STATS */}
-          {/* <section className='mt-6'>
-            <h3 className='text-md leading-6 font-medium text-gray-500'>
-              Last 30 days
-            </h3>
-            <dl className='mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3'>
-              <div className='bg-white overflow-hidden shadow rounded-lg'>
-                <div className='px-4 py-5 sm:p-6'>
-                  <dt className='text-sm font-medium text-gray-500 truncate'>
-                    Total Income
-                  </dt>
-                  <dd className='mt-1 text-3xl font-semibold text-green-500'>
-                    $71,897.00
-                  </dd>
-                </div>
-              </div>
-              <div className='bg-white overflow-hidden shadow rounded-lg'>
-                <div className='px-4 py-5 sm:p-6'>
-                  <dt className='text-sm font-medium text-gray-500 truncate'>
-                    Outstanding
-                  </dt>
-                  <dd className='mt-1 text-3xl font-semibold text-gray-900'>
-                    58.16%
-                  </dd>
-                </div>
-              </div>
-              <div className='bg-white overflow-hidden shadow rounded-lg'>
-                <div className='px-4 py-5 sm:p-6'>
-                  <dt className='text-sm font-medium text-gray-500 truncate'>
-                    Avg. Click Rate
-                  </dt>
-                  <dd className='mt-1 text-3xl font-semibold text-gray-900'>
-                    24.57%
-                  </dd>
-                </div>
-              </div>
-            </dl>
-          </section> */}
           {/* MAIN SECTION */}
           <section className='grid grid-cols-1 gap-6 sm:gap-6 md:grid-cols-3 mt-6'>
             <div className='col-span-2'>
-              {/* This example requires Tailwind CSS v2.0+ */}
-              <div className='bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 mb-6'>
-                {/* This example requires Tailwind CSS v2.0+ */}
+              {/* Earnings */}
+              <div className='bg-white overflow-hidden shadow sm:rounded-lg mb-6'>
+                <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
+                  <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                    Earnings
+                  </h3>
+                </div>
+                <div className='px-4 py-5 sm:p-6'>
+                  {/* Content goes here */}
+                  <div className='grid grid-cols-1 gap-6 sm:gap-4 md:grid-cols-3 '>
+                    <div>
+                      <h1>Outstanding</h1>
+                      <p>$0.00</p>
+                    </div>
+                    <div>
+                      <h1>Paid</h1>
+                      <p>$2000.00</p>
+                    </div>
+                    <div>
+                      <h1>Total</h1>
+                      <p className='text-green-400'> $2000.00</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              {/* Tasks */}
+              <div className='bg-white overflow-hidden shadow rounded-lg mb-6'>
                 <div className='bg-white px-4 py-4 border-b border-gray-200 sm:px-6'>
                   <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
                     <div className='ml-4 mt-2'>
                       <h3 className='text-lg leading-6 font-medium text-gray-900'>
-                        Projects
+                        Upcoming Tasks
                       </h3>
                     </div>
                     <div className='ml-4 mt-2 flex-shrink-0'>
                       <button
                         type='button'
-                        className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                        className='relative inline-flex items-center px-2 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                       >
-                        +
+                        <FaPlus />
                       </button>
                     </div>
                   </div>
@@ -101,33 +87,7 @@ function Dashboard({ session }) {
                   {/* Content goes here */}
                 </div>
               </div>
-
-              <div className='bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 mb-6'>
-                {/* This example requires Tailwind CSS v2.0+ */}
-                <div className='bg-white px-4 py-4 border-b border-gray-200 sm:px-6'>
-                  <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
-                    <div className='ml-4 mt-2'>
-                      <h3 className='text-lg leading-6 font-medium text-gray-900'>
-                        Tasks
-                      </h3>
-                    </div>
-                    <div className='ml-4 mt-2 flex-shrink-0'>
-                      <button
-                        type='button'
-                        className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
-                      >
-                        +
-                      </button>
-                    </div>
-                  </div>
-                </div>
-
-                <div className='px-4 py-5 sm:p-6'>
-                  {/* Content goes here */}
-                </div>
-              </div>
-              <div className='bg-white overflow-hidden shadow rounded-lg divide-y divide-gray-200 mb-6'>
-                {/* This example requires Tailwind CSS v2.0+ */}
+              <div className='bg-white overflow-hidden shadow rounded-lg mb-6'>
                 <div className='bg-white px-4 py-4 border-b border-gray-200 sm:px-6'>
                   <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
                     <div className='ml-4 mt-2'>
@@ -138,9 +98,9 @@ function Dashboard({ session }) {
                     <div className='ml-4 mt-2 flex-shrink-0'>
                       <button
                         type='button'
-                        className='relative inline-flex items-center px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                        className='relative inline-flex items-center px-2 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
                       >
-                        +
+                        <FaPlus />
                       </button>
                     </div>
                   </div>
@@ -151,18 +111,33 @@ function Dashboard({ session }) {
                 </div>
               </div>
             </div>
-
+            {/* Right Column */}
             <div className=''>
-              <div className='bg-white overflow-hidden shadow sm:rounded-lg mb-6'>
-                <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
-                  <h3 className='text-lg leading-6 font-medium text-gray-900'>
-                    Earnings
-                  </h3>
+              {/* Projects */}
+              <div className='bg-white overflow-hidden shadow rounded-lg mb-6'>
+                <div className='bg-white px-4 py-4 border-b border-gray-200 sm:px-6'>
+                  <div className='-ml-4 -mt-2 flex items-center justify-between flex-wrap sm:flex-nowrap'>
+                    <div className='ml-4 mt-2'>
+                      <h3 className='text-lg leading-6 font-medium text-gray-900'>
+                        Projects
+                      </h3>
+                    </div>
+                    <div className='ml-4 mt-2 flex-shrink-0'>
+                      <button
+                        type='button'
+                        className='relative inline-flex items-center px-2 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                      >
+                        <FaPlus />
+                      </button>
+                    </div>
+                  </div>
                 </div>
+
                 <div className='px-4 py-5 sm:p-6'>
                   {/* Content goes here */}
                 </div>
               </div>
+
               <div className='bg-white overflow-hidden shadow sm:rounded-lg'>
                 <div className='bg-white px-4 py-5 border-b border-gray-200 sm:px-6'>
                   <h3 className='text-lg leading-6 font-medium text-gray-900'>
