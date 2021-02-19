5b69d4af0738654d98e904234f44e5deb6e57b0b
Klaven Jones <klavensjones@gmail.com>
2021-02-19 01:33:56 -0500

    Working on Dashboard

--- pages/dashboard/clients.js
+++ pages/dashboard/clients.js
@@ -11,91 +11,85 @@ import {
 import { verifyIdToken } from '../../util/firebase/firebaseAdmin'
 import firebaseClient from '../../util/firebase/firebaseClient'
 
-function Clients({ session }) {
+function Clients() {
   const [focused, setFocus] = React.useState(false)
-  const [showMenu, setShow] = React.useState(true)
+  const [showMenu, setShow] = React.useState(false)
   const [modalType, setType] = React.useState('')
 
-  firebaseClient()
-
-  if (session) {
-    return (
-      <>
-        <Modal handleModal={setShow} show={showMenu} type={modalType} />
-        <SideNav page='clients'>
-          {/* Section title */}
-          <SectionHeader title='Clients' />
-          {/* Search Bar */}
-          <div className='pb-5 mb-10 sm:-ml-4 sm:flex sm:items-center sm:justify-between'>
-            <div
-              className={`mt-3 sm:mt-0 sm:ml-4 transition-all duration-800 ${
-                focused ? 'sm:w-96 md:w-96' : 'sm:w-80 md:w-80'
-              }`}
-            >
-              <label htmlFor='search_candidate' className='sr-only'>
-                Search
-              </label>
-              <div className='flex rounded-md shadow-sm'>
-                <div className='relative flex-grow focus-within:z-10'>
-                  <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
-                    {/* Heroicon name: search */}
-                    <HiOutlineSearch className='h-5 w-5 text-gray-400' />
-                  </div>
-                  <input
-                    type='text'
-                    name='search_candidate'
-                    id='search_candidate'
-                    className='focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:hidden border-gray-300'
-                    placeholder='Search'
-                  />
-                  <input
-                    type='text'
-                    name='search_candidate'
-                    id='search_candidate'
-                    className='hidden focus:ring-indigo-500 focus:border-indigo-500 w-full rounded-none rounded-l-md pl-10 sm:block sm:text-sm border-gray-300'
-                    placeholder='Search Clients'
-                    onFocus={() => setFocus(true)}
-                    onBlur={() => setFocus(false)}
-                  />
+  return (
+    <>
+      <Modal handleModal={setShow} show={showMenu} type={modalType} />
+      <SideNav page='clients'>
+        {/* Section title */}
+        <SectionHeader title='Clients' />
+        {/* Search Bar */}
+        <div className='pb-5 mb-10 sm:-ml-4 sm:flex sm:items-center sm:justify-between'>
+          <div
+            className={`mt-3 sm:mt-0 sm:ml-4 transition-all duration-800 ${
+              focused ? 'sm:w-96 md:w-96' : 'sm:w-80 md:w-80'
+            }`}
+          >
+            <label htmlFor='search_candidate' className='sr-only'>
+              Search
+            </label>
+            <div className='flex rounded-md shadow-sm'>
+              <div className='relative flex-grow focus-within:z-10'>
+                <div className='absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none'>
+                  {/* Heroicon name: search */}
+                  <HiOutlineSearch className='h-5 w-5 text-gray-400' />
                 </div>
-                <button
-                  type='button'
-                  className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
-                >
-                  {/* Heroicon name: sort-ascending */}
-                  <HiSortAscending className='h-5 w-5 text-gray-400' />
-                  <span className='ml-2'>Sort</span>
-                  {/* Heroicon name: chevron-down */}
-
-                  <HiOutlineChevronDown className='ml-2.5 -mr-1.5 h-5 w-5 text-gray-400' />
-                </button>
+                <input
+                  type='text'
+                  name='search_candidate'
+                  id='search_candidate'
+                  className='focus:ring-indigo-500 focus:border-indigo-500 block w-full rounded-none rounded-l-md pl-10 sm:hidden border-gray-300'
+                  placeholder='Search'
+                />
+                <input
+                  type='text'
+                  name='search_candidate'
+                  id='search_candidate'
+                  className='hidden focus:ring-indigo-500 focus:border-indigo-500 w-full rounded-none rounded-l-md pl-10 sm:block sm:text-sm border-gray-300'
+                  placeholder='Search Clients'
+                  onFocus={() => setFocus(true)}
+                  onBlur={() => setFocus(false)}
+                />
               </div>
-            </div>
-            <div className='mt-3 sm:mt-0 sm:ml-4'>
               <button
-                onClick={() => {
-                  setType('client')
-                  setShow(true)
-                }}
                 type='button'
-                className='inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                className='-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-r-md text-gray-700 bg-gray-50 hover:bg-gray-100 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
               >
-                Add New Client
+                {/* Heroicon name: sort-ascending */}
+                <HiSortAscending className='h-5 w-5 text-gray-400' />
+                <span className='ml-2'>Sort</span>
+                {/* Heroicon name: chevron-down */}
+
+                <HiOutlineChevronDown className='ml-2.5 -mr-1.5 h-5 w-5 text-gray-400' />
               </button>
             </div>
           </div>
-
-          {/* List Groups */}
-          <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
-            {/* List Item */}
-            <ListComponent data={Client} listType='client' />
+          <div className='mt-3 sm:mt-0 sm:ml-4'>
+            <button
+              onClick={() => {
+                setType('client')
+                setShow(true)
+              }}
+              type='button'
+              className='inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+            >
+              Add New Client
+            </button>
           </div>
-        </SideNav>
-      </>
-    )
-  } else {
-    return <h1>LOADING.....</h1>
-  }
+        </div>
+
+        {/* List Groups */}
+        <div className='grid grid-cols-1 gap-4 sm:grid-cols-2'>
+          {/* List Item */}
+          <ListComponent data={Client} listType='client' />
+        </div>
+      </SideNav>
+    </>
+  )
 }
 
 export async function getServerSideProps(context) {
